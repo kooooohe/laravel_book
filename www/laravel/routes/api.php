e699e0a7bed416115edcdabee0b41f71a2fa152a
kooooohe <1211lta+github@gmail.com>
2018-02-07 22:02:40 +0900

    delete unnecessary route of api

--- www/laravel/routes/api.php
+++ www/laravel/routes/api.php
@@ -15,9 +15,6 @@ use Illuminate\Http\Request;
 
 Route::get('/books', 'BookController@show');
 Route::post('/books', 'BookController@show');
-//Route::post('/addbook', 'AddBookController@addbook');
-//Route::get('/books/lend', 'BookController@lend');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    //Route::post('/books/lend', 'BookController@lend');
     return $request->user();
 });
